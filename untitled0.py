832c9d93c803742cac9ca67483979a8f4e478d21
nikolz_lin_Home <nikolzp@gmail.com>
2016-11-29 21:38:43 +0200

    1

---
+++ untitled0.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 21:38:37 2016
+
+@author: nikolz
+"""
+import turtle
+
+def gotoxy(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+
+def drav_line(from_x, from_y, to_x, to_y):
+    gotoxy(from_x, from_y)
+    tirtle.goto(to_x, to_y)
+        
+    
+
+
+
+
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(120, 120)
+
+turtle.pendown()
+turtle.circle(30)
