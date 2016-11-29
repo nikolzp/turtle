71f5d86632aa97a064eef57c45019d147b885ebd
nikolz_lin_Home <nikolzp@gmail.com>
2016-11-29 22:25:38 +0200

    1

--- untitled0.py
+++ untitled0.py
@@ -11,19 +11,26 @@ def gotoxy(x, y):
     turtle.goto(x, y)
     turtle.pendown()
 
-def drav_line(from_x, from_y, to_x, to_y):
+def draw_line(from_x, from_y, to_x, to_y):
     gotoxy(from_x, from_y)
-    tirtle.goto(to_x, to_y)
-        
-    
+    turtle.goto(to_x, to_y)
+       
+def draw_circle(x, y, z):
+    gotoxy(x, y)
+    turtle.circle(z)
 
+turtle.speed(1)     
+draw_line(-160, -100, -160, 80)
+draw_line(-160, 80, -80, 80)
+draw_line(-160, 40, -120, 80)
+draw_line(-100, 80, -100, 40)
+draw_circle(-100, 0, 20)
+draw_line(-100, 0, -100, -50)
+draw_line(-100, -10, -120, -20)
+draw_line(-100, -10, -80, -20)
+draw_line(-100, -50, -120, -60)
+draw_line(-100, -50, -80, -60)
 
+gotoxy(-200, 250)
+turtle.write('zagadayte chislo ot 1 do 100')
 
-
-turtle.circle(30)
-
-turtle.penup()
-turtle.goto(120, 120)
-
-turtle.pendown()
-turtle.circle(30)
